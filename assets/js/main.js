6c94b191cfa820040f2f937e49eaa07ce59f9bac
Alex King <alkin@amazon.com>
2019-09-30 08:29:29 -0500

    Implement RSVP form.

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,16 +31,7 @@
 			});
 
 			//Add form validation
-			$('#rsvp-form').on('submit', function() {
-				const eatHoney = Array.from(document.getElementsByClassName('awesome'))
-					.filter(item => item.value != '')
-					.length > 0;
-
-				if (eatHoney) {
-					location.reload();
-					return false;
-				}
-			});
+			$('#rsvp-form').on('submit', handleFormSubmit);
 
 			$('.flip-toggle').on('click', function() {
 				const $this = $(this);
@@ -266,6 +257,122 @@
 
 				});
 	});
+
+	function handleFormSubmit(event) {  // handles form submit without any jquery
+		event.preventDefault();
+		$('.form-overlay').removeClass('hide');      // we are submitting via xhr below
+		var form = event.target;
+		var formData = getFormData(form);
+		var isValid = validateForm(form);
+
+		if (!isValid) {
+			$('.form-overlay').addClass('hide');
+			return false;
+		}
+		var data = formData.data;
+
+		// If a honeypot field is filled, assume it was done so by a spam bot.
+		if (formData.honeypot) {
+			$('.form-overlay').addClass('hide');
+		  	return false;
+		}
+
+		var url = form.action;
+
+		var encoded = Object.keys(data).map(function(k) {
+		     return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+		}).join('&');
+
+		$.ajax({
+		    url: url,
+		    type: 'post',
+		    data: data,
+		    dataType: 'json',
+		    success: function(data, status, xhr) {
+				if (xhr.status === 200) {
+				    form.reset();
+
+					$('.rsvp-form').addClass('hide');
+					$('.rsvp-thanks').removeClass('hide');
+				} else {
+					$('.rsvp-error').removeClass('hide');
+				}
+			}
+		})
+		.fail(function() {
+		    $('rsvp-error').removeClass('hide)');
+		})
+		.always(function() {
+			$('.form-overlay').removeClass('hide');
+		});
+	}
+
+	function validateForm(form) {
+		var valid = true;
+		Array.from(form.elements).map(item => $(item))
+		.filter(function($item){
+			return $item.hasClass('required');
+		}).forEach(function($input){
+			const validationMsg = $input.parent().children('.form-validation-msg');
+			if ($input.val() === "") {
+				valid = false;
+				validationMsg.removeClass('invisible');
+			} else {
+				validationMsg.addClass('invisible');
+			}
+		});
+
+		return valid;
+	}
+
+	// get all data in form and return object
+	function getFormData(form) {
+		var elements = form.elements;
+		var honeypot;
+
+		var fields = Object.keys(elements).filter(function(k) {
+		  if (elements[k].className === "awesome") {
+			honeypot = elements[k].value;
+			return false;
+		  }
+		  return true;
+		}).map(function(k) {
+		  if(elements[k].name !== undefined) {
+			return elements[k].name;
+		  // special case for Edge's html collection
+		  }else if(elements[k].length > 0){
+			return elements[k].item(0).name;
+		  }
+		}).filter(function(item, pos, self) {
+		  return self.indexOf(item) == pos && item;
+	  	});
+
+		var formData = {};
+		fields.forEach(function(name){
+		  var element = elements[name];
+
+		  // singular form elements just have one value
+		  formData[name] = element.value;
+
+		  // when our element has multiple items, get their values
+		  if (element.length) {
+			var data = [];
+			for (var i = 0; i < element.length; i++) {
+			  var item = element.item(i);
+			  if (item.checked || item.selected) {
+				data.push(item.value);
+			  }
+			}
+			formData[name] = data.join(', ');
+		  }
+		});
+
+		// add form-specific values into the data
+		formData.formDataNameOrder = JSON.stringify(fields);
+		formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+
+		return {data: formData, honeypot: honeypot};
+	}
 
 })(jQuery);
 
